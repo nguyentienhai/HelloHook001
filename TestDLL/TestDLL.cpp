fd83478700e48af2553936836c614e21c7645360
Nguyễn Tiến Hải <Nguyen.TienHai@ymail.com>
2016-03-25 14:28:54 +0700

    step 1, initialize project & create DLL project

---
+++ TestDLL/TestDLL.cpp
@@ -0,0 +1,26 @@
+#include <Windows.h>
+
+// vung nho dung chung, chua bien handle cuar Hook
+#pragma data_seg("SHARED_DATA")
+HHOOK hGlobalHook = NULL;
+#pragma data_seg()
+
+// ham loc su kien nhan phim
+__declspec(dllexport) LRESULT CALLBACK FillKeyboard(int nHookCode, WPARAM wParam, LPARAM lParam)
+{
+	// neu su kien la nham phim va ma phim la Enter
+	if((nHookCode == HC_ACTION) && (wParam == 13))
+	{
+		MessageBox(0, "haint say hello :D", "HelloHook", 0);
+		return 1;
+	}
+
+	// goi Filter Function ke tiep trong chuoi cac Filter Function
+	return CallNextHookEx(hGlobalHook, nHookCode, wParam, lParam);
+}
+
+// ham an dinh bien hGlobal tai vung nho dung chung
+__declspec(dllexport) void SetGlobalHookHandle(HHOOK handleHook)
+{
+	hGlobalHook = handleHook;
+}
